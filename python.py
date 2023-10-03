e2c2db2ab27b85c787c22c02a3b85ccb09d71dc2
mcsaavedra <mcsaavedra23@gmail.com>
2023-10-03 14:06:55 -0300

    Se llama al formato JSON y se transforma en variable , se automatiza la inyección de los 500 datos solicitados

--- python.py
+++ python.py
@@ -17,4 +17,25 @@ cursor.execute('''
         Phone INTEGER
     )
 ''')
+#Carga de datos desde el archivo JSON
+
+with open('MOCK_DATA.json', 'r') as archivo:
+    datos_json = json.load(archivo)
+    
+#Insertar datos de registro en tablas usando bucle for para automatizar y dejando la condición de llegar hasta 500 
+
+for id,dato in enumerate(datos_json):
+    if id < 500:
+        cursor.execute('''
+        INSERT INTO Project_UC (FirstName, LastName, Email, Gender, PlanDeSalud, Phone)
+        VALUES (?, ?, ?, ?, ?, ?)
+      ''', (dato['first_name'], dato['last_name'], dato['email'], dato['gender'], dato['Plan de Salud'], dato['phone']))
+    else:
+        break
+    
+# Realiza un commit para guardar los cambios en la base de datos
+conexion.commit()
+
+# Cierra la conexión a la base de datos cuando hayas terminado
+conexion.close()
 
